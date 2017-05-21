dc2b2b73292a228ed123bae454847892efa2d50f
Sue Irvine <Suie1@users.noreply.github.com>
2017-05-21 13:23:35 -0400

    Create PeopleListController.js
    The Javascript controller for the component

---
+++ PeopleListController.js
@@ -0,0 +1,39 @@
+({
+    doInit : function(component, event) {
+        var searchtype = component.get("v.searchtype");
+        console.log(searchtype);
+        var action = component.get("c.getUsers");
+        action.setCallback(this, function(a) {
+            component.set("v.users", a.getReturnValue());
+            //component.set("v.members", a.getReturnValue());
+        });
+        $A.enqueueAction(action);
+        
+        var action2 = component.get("c.getURLPrefix");
+        action2.setCallback(this, function(b) {
+            component.set("v.urlprefix", b.getReturnValue());
+        });
+        $A.enqueueAction(action2);
+    },
+    
+    searchKeyChange: function(component, event) {
+        var searchtype = component.get("v.searchtype");
+        console.log(searchtype);
+        var searchKey = event.getParam("searchKey");
+        var action = component.get("c.searchProfile");
+        if (searchtype=="Name"){
+            console.log("findbyName");
+        	action = component.get("c.findbyName");
+    	}
+      
+        //var action = component.get("c.searchNetworkProfile");
+        action.setParams({
+          "searchKey": searchKey
+        });
+        action.setCallback(this, function(a) {
+            component.set("v.users", a.getReturnValue());
+            //component.set("v.members", a.getReturnValue());
+        });
+        $A.enqueueAction(action);
+	}
+})
